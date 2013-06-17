8bcae451282178c77826ec812cd321b804992a55
Jordan McConnell <jordanmcconnell@comcast.net>
2013-06-16 23:52:58 -0700

    added unicode functions to models, also added foreign key to members on fine

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -35,15 +36,21 @@ YEAR_IN_SCHOOL_CHOICES = (
 class DaysOfWeek(models.Model):
     day = models.CharField(max_length=8)
 
+    def __unicode__(self):
+        return self.day
+
 class Member(models.Model):
     """
     Model for a Lodge Member of Beaver Lodge
     """
+    user = models.OneToOneField(User)
     nickname = models.CharField(max_length=30, blank=True, null=True)
     member_since = models.DateField(null=True, blank=True)
     year = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
     workjobs = models.ManyToManyField('WorkJob', through='ScheduledWorkJob')
     positions = models.ManyToManyField('Position', through='PositionHeld')
+    def __unicode(self):
+        return self.user.username
 
     def GetCurrentPositions():
         """
@@ -59,6 +66,9 @@ class WorkJobResponsibility(models.Model):
     description = models.CharField(max_length=200)
     priority = models.IntegerField()
 
+    def __unicode__(self):
+        return str(self.priority) + ": " + self.description
+
 class WorkJob(models.Model):
     """
     Model to define all the work jobs at beaver lodge, on a particular day a job must be started at a certain time and completed by a certain time
@@ -69,6 +79,9 @@ class WorkJob(models.Model):
     time_due = models.TimeField()
     length = models.IntegerField()
     days = models.ManyToManyField('DaysOfWeek')
+
+    def __unicode__(self):
+        return self.title
 
 
 class Position(models.Model):
@@ -81,6 +94,9 @@ class Position(models.Model):
     is_exec = models.BooleanField()
     percent_off_workjobs = models.IntegerField()
 
+    def __unicode__(self):
+        return self.title
+
 class PositionHeld(models.Model):
     """
     Intermediary model for representing the positions that members have held in the past
@@ -91,13 +107,19 @@ class PositionHeld(models.Model):
     end_term = models.ForeignKey('Term', related_name='+')
     is_active = models.BooleanField()
 
+    def __unicode__(self):
+        return self.position + " : " + self.member
+
 class Term(models.Model):
     """
     Collection of terms representing school
     """
     begin_date = models.DateField()
     end_date = models.DateField()
-    school_term = models.CharField(max_length = 1)
+    school_term = models.CharField(max_length = 1, choices=SCHOOL_TERM)
+
+    def __unicode__(self):
+        return self.get_school_term_display + " " + begin_date.year
 
 class ScheduledWorkJob(models.Model):
     """
@@ -106,6 +128,9 @@ class ScheduledWorkJob(models.Model):
     date_due = models.DateField()
     member = models.ForeignKey('Member')
     workjob = models.ForeignKey('WorkJob')
+
+    def __unicode__(self):
+        return self.workjob + " by " + self.member + " on " + str(date_due)
 
 class Trade(models.Model):
     """
@@ -121,11 +146,15 @@ class Trade(models.Model):
     approved_date = models.DateTimeField(null=True)
     is_permanent_trade = models.BooleanField(default=False)
 
+    def __unicode__(self):
+        return self.proposed_by_workjob
+
 class Fine(models.Model):
     """
     When a member fails to meet their duties for a work job or house duties they can be assigned a fine which must be paid in full to the Financial manager
     """
     offense = models.CharField(max_length = 50)
+    member = models.ForeignKey('Member')
     amount = models.IntegerField(default = 0)
     paid = models.BooleanField(default = False)
 
@@ -138,6 +167,9 @@ class Fine(models.Model):
         else:
             return amount
 
+    def __unicode__(self):
+        return self.member + " owes " + str(self.amount)
+
 class MakeUpJob(models.Model):
     """
     When a member fails a job they may be asked to perform a work up job
@@ -148,3 +180,6 @@ class MakeUpJob(models.Model):
     date_due = models.DateField()
     fine = models.ForeignKey('Fine')
     completed = models.BooleanField(default = False)
+
+    def __unicode__(self):
+        return self.title
